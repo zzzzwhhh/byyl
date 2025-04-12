a6402dc16ab68072601a0e1e98bd9c85f50614ec
zenglj <zenglj@live.com>
2025-04-13 00:22:37 +0800

    先改名CMakePreset.json，运行bison输出，允许调试

--- frontend/flexbison/FlexBisonExecutor.cpp
+++ frontend/flexbison/FlexBisonExecutor.cpp
@@ -29,7 +29,9 @@ bool FlexBisonExecutor::run()
     }
 
     // 如果要查看LALR的移进与归约过程，请设置yydebug为1
-    // yydebug = 1;
+#ifdef BISON_DEBUG_ENABLE
+    yydebug = 1;
+#endif
 
     // 词法、语法分析生成抽象语法树AST
     bool result = yyparse();
